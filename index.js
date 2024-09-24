b38920000afe854cf6e6f9c8b43b6217029a5407
Maria Janaina Silva Ferreira <Maria.Ferreira@fit-tecnologia.org.br>
2024-09-24 14:18:01 -0300

    translating

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const contactRoute = require('./src/routes/contactRoute')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require ('./swagger-output.json')
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/docs', swaggerUi.serve,swaggerUi.setup(swaggerFile))
 app.use(contactRoute)
